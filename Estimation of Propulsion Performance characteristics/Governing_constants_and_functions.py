c8098f7903803073a7cffd751c95e4f8848f1a37
Mariano <nano2598.r@gmail.com>
2020-05-29 18:23:52 +0200

    Added more constants and functions to the governing file, started on the problem 1 file

--- Estimation of Propulsion Performance characteristics/Governing_constants_and_functions.py
+++ Estimation of Propulsion Performance characteristics/Governing_constants_and_functions.py
@@ -11,11 +11,30 @@ C_fd = 0.015
 alpha_0 = 0
 K_0 = 6.11
 
-Bp = 2 # Nunmber of blades, optimal is 2 from research
-Dp = 0.2794 # Propeller diameter in m
-Hp = 0.1194 # Propeller pitch in m
-Gp = 0.01502525 * 9.81 # Weight of propeller in N
+# Environment Parameters
+Temp = 288.15 # ISA sea-level Temperature in Celsius
+p = 101325 # ISA sea-level pressure in Pa
+g = 9.81 # Acceleration due to gravity m/s^2
+R = 287.05
+rho = p/(R*Temp) # ISA sea-level density kg/m^3
 
-def f_C_T (Bp = Bp, Dp = Dp, Hp = Hp, Gp = Gp):
-    return 0.25 * np.pi**3 * zeta ** 2 * Bp * K_0 * (epsilon * np.arctan(Hp/(np.pi * Dp))-alpha_0)/(np.pi * A + K_0)
+# General Parameters
+W = 3.7 * g
+n_r = 4
 
+# Propeller parameters
+B_p = 2 # Nunmber of blades, optimal is 2 from research
+D_p = 0.2794 # Propeller diameter in m
+H_p = 0.1194 # Propeller pitch in m
+W_p = 0.01502525 * g # Weight of propeller in N
+
+# Motor parameters
+K_V0 =
+
+C_d = C_fd + ((np.pi * A * K_0 ** 2) * (epsilon * np.arctan(H_p/(np.pi * D_p))-alpha_0) ** 2)/(e * (np.pi * A + K_0) ** 2)
+
+def f_C_T (B_p = B_p, D_p = D_p, H_p = H_p, W_p = W_p):
+    return 0.25 * np.pi**3 * zeta ** 2 * B_p * K_0 * (epsilon * np.arctan(H_p/(np.pi * D_p))-alpha_0)/(np.pi * A + K_0)
+
+def f_C_M (B_p = B_p, D_p = D_p, H_p = H_p, W_p = W_p):
+    return (1/8 * A) * np.pi ** 2 * C_d * zeta ** 2 * lambd * B_p ** 2
--- Estimation of Propulsion Performance characteristics/Problem_1.py
+++ Estimation of Propulsion Performance characteristics/Problem_1.py
@@ -1,3 +1,9 @@
-from Governing_constants_and_functions.py import f_C_T
+import numpy as np
+import Governing_constants_and_functions as G
 
-print(f_C_T)
+T = G.W/G.n_r # Thrust (N) in hover is equal to weight (N) by number of rotors
+
+N = 60 * np.sqrt(T/(G.rho * G.D_p ** 4 * G.f_C_T())) # Motor speed (RPM)
+M = G.rho * G.D_p ** 5 * G.f_C_M * (N/60) ** 2 # Propeller torque in N*m
+
+
