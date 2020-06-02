0eb4dc957b5a3d97242d7f66b5dd79b5d0d60608
abovearth <roeland_desmet@hotmail.com>
2020-06-02 16:43:01 +0200

    Wrote a script that updates the center of gravity based on a weighted average

--- Mass moment of inertia/spreadsheet.py
+++ Mass moment of inertia/spreadsheet.py
@@ -14,4 +14,24 @@ sheet = client.open("Mass of components for mass moment inertia calculation").sh
 
 # Extract and print all of the values
 list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+#print(list_of_hashes)
+
+#the data is stored in list of hashes as a list containing dict(ionarie)s by the import script
+
+#calculate center of gravity using the weighted average
+cgx = 0.0
+cgy = 0.0
+cgz = 0.0
+mass = 0.0
+
+for column in list_of_hashes:
+    cgx = cgx + (column.get('mass [kg]')*column.get('x [m]'))
+    cgy = cgy + (column.get('mass [kg]')*column.get('y [m]'))
+    cgz = cgz + (column.get('mass [kg]')*column.get('z [m]'))
+    mass = mass + column.get('mass [kg]')
+
+center_of_gravity = (cgx/mass, cgy/mass, cgz/mass)
+
+#update the corresponding cell Row 2 Column G
+sheet.update_cell(2, 7, str(center_of_gravity[0])+","+str(center_of_gravity[1])+","+str(center_of_gravity[2]))
+
