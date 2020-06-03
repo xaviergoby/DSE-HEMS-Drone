fffc107380b202f0af91ad2df5b5d4adb295278e
abovearth <roeland_desmet@hotmail.com>
2020-06-03 15:48:21 +0200

    Rewrote the script to have functions

--- Mass moment of inertia/spreadsheet.py
+++ Mass moment of inertia/spreadsheet.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any
+from typing import Optional, Any, Dict, List, Union
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -14,102 +14,122 @@ def normalize(v):
     return v / norm
 
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+def import_spreadsheet(sheetname):
+    # the data is stored in list of hashes as a list containing dict(ionarie)s by the import script
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("Mass of components for mass moment inertia calculation").sheet1
+    # Find a workbook by name and open the first sheet
+    # Make sure you use the right name here.
+    sheet = client.open(sheetname).sheet1
+    return sheet
 
+
+sheet = import_spreadsheet("Mass of components for mass moment inertia calculation")
 # Extract and print all of the values
 list_of_hashes = sheet.get_all_records()
+
+
 # print(list_of_hashes)
 
-# the data is stored in list of hashes as a list containing dict(ionarie)s by the import script
+def center_of_gravity(m, x, y, z):
+    # calculate center of gravity using the weighted average
+    cgx = 0.0
+    cgy = 0.0
+    cgz = 0.0
 
-# calculate center of gravity using the weighted average
-cgx = 0.0
-cgy = 0.0
-cgz = 0.0
-mass = 0.0
+    for i in range(len(m)):
+        cgx = cgx + (m[i] * x[i])
+        cgy = cgy + (m[i] * y[i])
+        cgz = cgz + (m[i] * z[i])
+    # divide by sum of mass
+    return float(cgx / sum(m)), float(cgy / sum(m)), float(cgz / sum(m))
 
-for column in list_of_hashes:
-    cgx = cgx + (column.get('mass [kg]') * column.get('x [m]'))
-    cgy = cgy + (column.get('mass [kg]') * column.get('y [m]'))
-    cgz = cgz + (column.get('mass [kg]') * column.get('z [m]'))
-    mass = mass + column.get('mass [kg]')
-# divide by sum of mass
-cgx, cgy, cgz = cgx / mass, cgy / mass, cgz / mass
-# format the center of gravity into a dict
-center_of_gravity = {'x': cgx,
-                     'y': cgy,
-                     'z': cgz}
+
+#
+m = np.array(sheet.get('B2:B999'), dtype=float)
+x = np.array(sheet.get('C2:C999'), dtype=float)
+y = np.array(sheet.get('D2:D999'), dtype=float)
+z = np.array(sheet.get('E2:E999'), dtype=float)
+
+
+c_o_g = center_of_gravity(m, x, y, z)
 # update the corresponding cell G2
-sheet.update('J2', str(center_of_gravity))
+sheet.update('J2', str(c_o_g))
+
 
 # update the positions of components with respect to the center of gravity
 
-# This is the position of the components with respect to the body axis system: origin is the Center of gravity,
-# x lies in the symmetry plane and points forward, z lies in the symmetry plane and points downward, y from the right
-# handrule
+
+def vehicle_to_body_reference_frame(x_v, y_v, z_v, cg):
+    """This is the position of the components with respect to the body axis system: origin is the Center of gravity,
+    x lies in the symmetry plane and points forward, z lies in the symmetry plane and points downward, y from the
+    right handrule
+
+   """
+    x_b = cg[0] - y_v
+    y_b = cg[1] - x_v
+    z_b = cg[2] + z_v
+    return x_b, y_b, z_b
+
+
 cellvalues = []
 for column in list_of_hashes:
-    column['Xb [m]'] = center_of_gravity.get('x') - column.get('y [m]')
-    column['Yb [m]'] = center_of_gravity.get('y') - column.get('x [m]')
-    column['Zb [m]'] = center_of_gravity.get('z') + column.get('z [m]')
-    # format for cellupdate
-    cellvalues.append([column['Xb [m]'], column['Yb [m]'], column['Zb [m]']])
+    Xb, Yb, Zb = vehicle_to_body_reference_frame(column.get("x [m]"), column.get("y [m]"), column.get("z [m]"),c_o_g)
+    cellvalues.append([Xb, Yb, Zb])
 
 # update the cells in the sheet
 sheet.update('K2', cellvalues)
 
-# Calculate mass moment of inertia for each piece
+
+def mass_moment_of_inertia_cuboid(mass, width, depth, height):
+    """
+    This function calculates ths mass moment of inertia for a cuboid. It assumes an equal distribution of mass
+    throughout the specified cuboid. It returns an inertia tensor.
+     :param float mass: mass
+     :param float width: width
+     :param float depth: depth
+     :param float height: height
+     :return: nparray inertia tensor
+    """
+    return 1. / 12. * np.array([[mass * (height ** 2 + depth ** 2), 0.0, 0.0],
+                                [0.0, mass * (width ** 2 + depth ** 2), 0.0],
+                                [0.0, 0.0, mass * (width ** 2 + height ** 2)]])
+
+
+# Calculate mass moment of inertia for each component
 InertiaTensor = np.zeros((3, 3))
 for column in list_of_hashes:
     m = column.get('mass [kg]')
-
-#    if column['shape'] == 'Solid cuboid of width w, height h, depth d': # this piece will not be used. cuboids are the only option.
     w = column.get('w [m]')
     d = column.get('d [m]')
     h = column.get('h [m]')
-    """We will not orient the cubes they will all point up Keep it simple stupid. That's why this piece of code will be ignored
-        
-        # use orientationvector provided or use z-axis
-        try:
-            orientationVector = np.array(str(column.get('orientation vector')).split(','), dtype=float)
-        except:
-            orientationVector = np.array([0., 0., 1.])
-            print("For " + column.get("component name or description") + ", no orientation vector was found.")
-            print("Orientation vector [0., 0., 1.] was assumed.")
-        orientationVector = normalize(orientationVector)
-    """
-    InertiaTensor = InertiaTensor + 1. / 12. * np.array([[m * (h ** 2 + d ** 2), 0.0, 0.0],
-                                                         [0.0, m * (w ** 2 + d ** 2), 0.0],
-                                                         [0.0, 0.0, m * (w ** 2 + h ** 2)]])
+    InertiaTensor = InertiaTensor + mass_moment_of_inertia_cuboid(m, w, d, h)
 
-"""Cylinders will not be used for this estimation
-    elif column['shape'] == 'Solid cylinder of radius r, height h':
-        r = column.get('w or r [m]')
-        h = column.get('h [m]')
-        try:
-            orientationVector = np.array(str(column.get('orientation vector')).split(','), dtype=float)
-        except:
-            orientationVector = np.array([0., 0., 1.])
-            print("For " + column.get("component name or description") + ", no orientation vector was found.")
-            print("Orientation vector [0., 0., 1.] was assumed.")
-        orientationVector = normalize(orientationVector)
-"""
+
+def mass_moment_of_inertia_steiner(mass, x, y, z):
+    """
+
+    :param float mass: mass
+    :param float x:
+    :param float y:
+    :param float z:
+    :return: nparray inertia tensor
+    """
+    return np.array(
+        [[mass * x * x, mass * x * y, mass * x * z], [mass * y * x, mass * y * y, mass * y * z], [mass * z * x, mass * z * y, mass * z * z]])
+
+
 # calculate Steiner term
 for column in list_of_hashes:
     m = column.get('mass [kg]')
     x = column.get('Xb [m]')
     y = column.get('Yb [m]')
     z = column.get('Zb [m]')
-    InertiaTensor = InertiaTensor + np.array(
-        [[m * x * x, m * x * y, m * x * z], [m * y * x, m * y * y, m * y * z], [m * z * x, m * z * y, m * z * z]])
+    InertiaTensor = InertiaTensor + mass_moment_of_inertia_steiner(m, x, y, z)
 
 # update the cells in the sheet
 sheet.update('O5', InertiaTensor.tolist())
