6690133f67c647635403a35546090a03cabeeaca
Paco Frantzen <paco.frantzen@ziggo.nl>
2020-06-03 11:11:05 +0200

    Addeed Template_test.py file as a template for testing different scripts

---
+++ Template_test.py
@@ -0,0 +1,18 @@
+import unittest
+import math
+import numpy as np
+class MyTestCase(unittest.TestCase):
+
+    def basic_test(self):
+        a = 5
+        b = 5
+        self.assertEqual(a, b)
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    unittest.main()
