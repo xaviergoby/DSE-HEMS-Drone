bdd92d926e4b31e646d9416479b4c41013d94887
Paco Frantzen <paco.frantzen@ziggo.nl>
2020-06-09 16:16:18 +0200

    added coords.txt to aid development outside of airsim and minor change to Sensor_simple.py

--- Sensors/Sensor_simple.py
+++ Sensors/Sensor_simple.py
@@ -1,21 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-rotate_speed = 2* 360 #deg/s
-resolution = 1.4 #deg
+rotate_speed = 360 #deg/s
+resolution = 18 #deg
 fov = 90 #deg
 pixels = fov / resolution
-sizes = [0.01, 0.1, 0.2, 0.5, 1]
 velocity = np.linspace(0,10,100) #m/s
 size = 0.1 #m
 fly_height = 3 #m
 
-def get_distance(size):
-    #input is the size of an object and output will be the minimum distance required to sense
-    return size / np.sin(np.deg2rad(resolution))
 
-distance_object = get_distance(size)
-# Spacing_measure is the spacing between a point when first scanned and when scanned again
-# from a different spot
-spacing_measure = np.sqrt(1.5 * 360 / (rotate_speed ) * velocity - fly_height)
 plt.plot(velocity, spacing_measure)
 plt.show()
