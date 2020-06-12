592ee60b950b7ca872c718b2985b58b2c0db1bfe
Mariano <nano2598.r@gmail.com>
2020-06-12 17:36:37 +0200

    Modified the overall model so that the angular velocity (N) and torque (M) are now functions in the Governing_constants_and_functions.py file instead of just being calculated in Hover_estimation.py. Fixed a mistake where I hadn't divided by number of rotors in Max_speed_and_range_estimation.py file.

--- Estimation of Propulsion Performance characteristics/Max_speed_and_range_estimation.py
+++ Estimation of Propulsion Performance characteristics/Max_speed_and_range_estimation.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 C1 = 3
 C2 = 1.5
 S = 0.1 # They never say what number they use for area which is really annoying so we'll have to experiment
-theta_max = 62 * np.pi/180 # max pitch angle, later we can just take this from the calcuation done in max_load_estimation
+theta_max = 40 * np.pi/180 # max pitch angle, later we can just take this from the calcuation done in max_load_estimation
 
 def C_D(theta):
     return C1 * (1 - (np.cos(theta)) ** 3) + C2 * (1 - (np.sin(theta)) ** 3)
@@ -16,10 +16,10 @@ def V (theta):
     return np.sqrt( (2 * G.W * np.tan(theta)) / (G.rho * S * C_D(theta)) )
 
 def N(theta):
-    return 60 * np.sqrt(G.W/(G.rho * G.n_r * G.D_p ** 4 * G.f_C_T() * np.cos(theta)))
+    return G.N(T=G.W/(G.n_r * np.cos(theta)))
 
 def M(theta):
-    return G.rho * G.D_p ** 5 * G.f_C_M() * (N(theta)/60) ** 2
+    return G.M(T=G.W/(G.n_r * np.cos(theta)), N=N(theta))
 
 def U_m(theta):
     return G.f_U_m(N=N(theta), M=M(theta))
@@ -53,13 +53,16 @@ Data_C_D = C_D(theta_values)
 
 #print(Data_V)
 #print(Data_T_fly)
+#print(Data_range)
 print('Max speed is', V(theta_max), 'm/s')
 print('Max range is', max(Data_range), 'm')
 
 indx = np.where(Data_range == max(Data_range))[0]
 print('Pitch for max range is', theta_values[indx] * 180/np.pi, 'degrees')
 print('Velocity for max range is', Data_V[indx], 'm/s')
-print(N(theta_values[indx]))
+print('RPM required is', N(theta_values[indx]))
 plt.plot(theta_values * 180/np.pi, Data_C_D)
+plt.plot()
 plt.show()
+
 print(C_D(0*np.pi/180))
